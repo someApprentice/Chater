dd1a37b6d9b6bda909da2a799cddf231e860011e
someApprentice <someApprentice@protonmail.com>
2023-08-03 17:06:46 +0700

    feature: add generated messages from ChatGPT

--- server/populate.ts
+++ server/populate.ts
@@ -14,6 +14,10 @@ import { User } from '../src/models/user';
 import { Dialog } from '../src/models/dialog';
 import { Message } from '../src/models/message';
 
+import ChatGPT_GENERATED_USERS from './ChatGPT_generated_users.json';
+import ChatGPT_GENERATED_MESSAGES from './ChatGPT_generated_messages.json';
+
+
 export async function populate() {
   let users: User[] = [] as User[];
   let dialogs: Dialog[] = [] as Dialog[];
@@ -42,64 +46,46 @@ export async function populate() {
 
   dialogs.push(publicDialog);
 
-  // old public messages
-  for (let i = 0; i < 60; i++) {
-    let d = new Date();
-    d = new Date(d.setDate(d.getDate() - 1));
-    d = new Date(d.setHours(d.getHours() - 1));
-    d = new Date(d.setSeconds(d.getSeconds() - i));
+  for (let u of ChatGPT_GENERATED_USERS) {
+    let id = nanoid();
 
-    let message: Message = {
-      id: nanoid(),
-      dialog: publicDialog.id,
-      author: user.id,
-      date: Math.floor(d.getTime() / 1000),
-      content: "Old Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    } as Message;
+    let avatar = await generate(250, 250, id);
 
-    messages.push(message);
+    let user: User = {
+      id,
+      email: `${u}@chater.com`,
+      name: u,
+      hash: await hash('password'),
+      avatar
+    };
 
-    publicDialog.messages_count++;
+    users.push(user);
   }
 
-  // public messages
-  for (let i = 0; i < 20; i++) {
-    let d = new Date();
-    d = new Date(d.setDate(d.getDate() - 1));
-    d = new Date(d.setSeconds(d.getSeconds() + i));
+  let days = ChatGPT_GENERATED_MESSAGES.length;
 
-    let message: Message = {
-      id: nanoid(),
-      dialog: publicDialog.id,
-      author: user.id,
-      date: Math.floor(d.getTime() / 1000),
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    } as Message;
+  for (let conversation of ChatGPT_GENERATED_MESSAGES) {
+    for (let i in conversation.messages) {
+      let d = new Date();
+      d = new Date(d.setDate(d.getDate() - (days - conversation.day)));
+      d = new Date(d.setSeconds(d.getSeconds() - (conversation.messages.length - Number(i))));
 
-    messages.push(message);
+      let u = users.find(u => u.name == conversation.messages[i].author);
 
-    publicDialog.messages_count++;
+      let message: Message = {
+        id: nanoid(),
+        dialog: publicDialog.id,
+        author: u!.id,
+        date: Math.floor(d.getTime() / 1000),
+        content: conversation.messages[i].content
+      } as Message;
+
+      messages.push(message);
+
+      publicDialog.messages_count++;
+    }
   }
 
-  // new public messages
-  for (let i = 0; i < 60; i++) {
-    let d = new Date();
-    d = new Date(d.setDate(d.getDate() - 1));
-    d = new Date(d.setHours(d.getHours() + 1));
-    d = new Date(d.setSeconds(d.getSeconds() + i));
-
-    let message: Message = {
-      id: nanoid(),
-      dialog: publicDialog.id,
-      author: user.id,
-      date: Math.floor(d.getTime() / 1000),
-      content: "New Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    } as Message;
-
-    messages.push(message);
-
-    publicDialog.messages_count++;
-  }
 
   for (let i = 0; i < 10; i++) {
     let id = nanoid();
